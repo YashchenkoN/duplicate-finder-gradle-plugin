5c653eadca2008c75fccb2f4270778faaa05cd43
kolyan <vkont4@gmail.com>
2018-09-29 14:00:00 +0200

    Gradle API

--- src/main/java/io/github/yashchenkon/duplicatefinder/DuplicateFinderPlugin.java
+++ src/main/java/io/github/yashchenkon/duplicatefinder/DuplicateFinderPlugin.java
@@ -1,7 +1,15 @@
 package io.github.yashchenkon.duplicatefinder;
 
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
 /**
  * @author Mykola Yashchenko
  */
-public class DuplicateFinderPlugin {
+public class DuplicateFinderPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(final Project project) {
+
+    }
 }
