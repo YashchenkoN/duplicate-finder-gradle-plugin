0c1921dbcb09e8f4a335cc19aed7701d8d1e114d
kolyan <vkont4@gmail.com>
2018-10-04 14:00:00 +0200

    Task method

--- src/main/java/io/github/yashchenkon/duplicatefinder/DuplicateFinderTask.java
+++ src/main/java/io/github/yashchenkon/duplicatefinder/DuplicateFinderTask.java
@@ -1,9 +1,15 @@
 package io.github.yashchenkon.duplicatefinder;
 
 import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
 
 /**
  * @author Mykola Yashchenko
  */
 public class DuplicateFinderTask extends DefaultTask {
+
+    @TaskAction
+    public void checkForDuplicateClasses() {
+
+    }
 }
