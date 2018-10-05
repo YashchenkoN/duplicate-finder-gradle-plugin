f27977f78e9ce347b1cb43a7fb5e26d14794f12c
kolyan <vkont4@gmail.com>
2018-10-05 14:00:00 +0200

    Impl task

--- src/main/java/io/github/yashchenkon/duplicatefinder/DuplicateFinderTask.java
+++ src/main/java/io/github/yashchenkon/duplicatefinder/DuplicateFinderTask.java
@@ -1,7 +1,20 @@
 package io.github.yashchenkon.duplicatefinder;
 
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
 import org.gradle.api.DefaultTask;
+import org.gradle.api.GradleException;
 import org.gradle.api.tasks.TaskAction;
+
+import lombok.SneakyThrows;
 
 /**
  * @author Mykola Yashchenko
@@ -9,7 +22,28 @@ import org.gradle.api.tasks.TaskAction;
 public class DuplicateFinderTask extends DefaultTask {
 
     @TaskAction
+    @SneakyThrows
     public void checkForDuplicateClasses() {
+        final Map<String, Set<String>> modulesByFile = new HashMap<>();
+        getProject().getConfigurations().forEach(configuration -> {
+            configuration.getResolvedConfiguration().getResolvedArtifacts().forEach(artifact -> {
+                final Enumeration<? extends ZipEntry> entries = new ZipFile(artifact.getFile()).entries();
+                while (entries.hasMoreElements()) {
+                    final ZipEntry entry = entries.nextElement();
+                    if (!entry.isDirectory() && !entry.getName().startsWith("META-INF/")) {
+                        final Set<String> modules = modulesByFile.getOrDefault(entry.getName(), new HashSet<>());
+                        modules.add(artifact.getModuleVersion().toString());
+                        modulesByFile.put(entry.getName(), modules);
+                    }
+                }
+            });
+        });
 
+        final List<Map.Entry<String, Set<String>>> duplicated = modulesByFile.entrySet().stream()
+                .filter(e -> e.getValue().size() > 1)
+                .collect(Collectors.toList());
+        if (!duplicated.isEmpty()) {
+            throw new GradleException("Found duplicate entries");
+        }
     }
 }
