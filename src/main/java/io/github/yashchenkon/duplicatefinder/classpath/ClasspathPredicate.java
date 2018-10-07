5f80c6918dfaa8a41ed14d0d73700b5073322619
kolyan <vkont4@gmail.com>
2018-10-07 14:00:00 +0200

    Added predicate

--- src/main/java/io/github/yashchenkon/duplicatefinder/classpath/ClasspathPredicate.java
+++ src/main/java/io/github/yashchenkon/duplicatefinder/classpath/ClasspathPredicate.java
@@ -1,7 +1,32 @@
 package io.github.yashchenkon.duplicatefinder.classpath;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 /**
  * @author Mykola Yashchenko
  */
-public class ClasspathPredicat {
+public class ClasspathPredicate implements Predicate<String> {
+
+    private final List<Pattern> patterns;
+
+    public ClasspathPredicate(final String... patterns) {
+        this(Arrays.asList(patterns));
+    }
+
+    public ClasspathPredicate(final Collection<String> patterns) {
+        this.patterns = patterns.stream()
+                .map(s -> Pattern.compile(s, Pattern.CASE_INSENSITIVE))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean test(final String value) {
+        return patterns.stream()
+                .anyMatch(p -> p.matcher(value).matches());
+    }
 }
