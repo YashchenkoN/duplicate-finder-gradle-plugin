1d3da72572fd8b6259719b31ec754cc9b1c1ee85
kolyan <vkont4@gmail.com>
2018-10-11 14:00:00 +0200

    Filter to implement predicate

--- src/main/java/io/github/yashchenkon/duplicatefinder/classpath/ClasspathFilter.java
+++ src/main/java/io/github/yashchenkon/duplicatefinder/classpath/ClasspathFilter.java
@@ -1,7 +1,14 @@
 package io.github.yashchenkon.duplicatefinder.classpath;
 
+import java.util.function.Predicate;
+
 /**
  * @author Mykola Yashchenko
  */
-public class ClasspathFilter {
+public class ClasspathFilter implements Predicate<String> {
+
+    @Override
+    public boolean test(String s) {
+        return false;
+    }
 }
