9e85904175e7b9ecd78220814b5855ed17cfc47c
kolyan <vkont4@gmail.com>
2018-10-12 14:00:00 +0200

    Add default predicates

--- src/main/java/io/github/yashchenkon/duplicatefinder/classpath/ClasspathFilter.java
+++ src/main/java/io/github/yashchenkon/duplicatefinder/classpath/ClasspathFilter.java
@@ -7,8 +7,30 @@ import java.util.function.Predicate;
  */
 public class ClasspathFilter implements Predicate<String> {
 
+    private static final ClasspathPredicate DEFAULT_IGNORED_RESOURCES_PREDICATE = new ClasspathPredicate(
+            "^META-INF/.*",
+            "^OSGI-INF/.*",
+            "^licenses/.*",
+            ".*license(\\.txt)?$",
+            ".*notice(\\.txt)?$",
+            ".*readme(\\.txt)?$",
+            ".*third-party(\\.txt)?$",
+            ".*package\\.html$",
+            ".*overview\\.html$");
+
+    private static final ClasspathPredicate DEFAULT_IGNORED_CLASS_PREDICATE = new ClasspathPredicate(
+            // this regex matches inner classes
+            ".*\\$.*",
+            "module-info");
+
+    private static final ClasspathPredicate DEFAULT_IGNORED_LOCAL_DIRECTORIES = new ClasspathPredicate(
+            "^.git$",
+            "^.svn$",
+            "^.hg$",
+            "^.bzr$");
+
     @Override
-    public boolean test(String s) {
+    public boolean test(final String value) {
         return false;
     }
 }
