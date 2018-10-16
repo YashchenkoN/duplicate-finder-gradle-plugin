1fa35d5201eeace223e807f96024ebe87196ade2
kolyan <vkont4@gmail.com>
2018-10-16 14:00:00 +0200

    Use filter

--- src/main/java/io/github/yashchenkon/duplicatefinder/DuplicateFinderTask.java
+++ src/main/java/io/github/yashchenkon/duplicatefinder/DuplicateFinderTask.java
@@ -15,6 +15,7 @@ import org.gradle.api.GradleException;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.tasks.TaskAction;
 
+import io.github.yashchenkon.duplicatefinder.classpath.ClasspathFilter;
 import lombok.SneakyThrows;
 
 /**
@@ -28,6 +29,8 @@ public class DuplicateFinderTask extends DefaultTask {
         final DuplicateFinderExtension extension = (DuplicateFinderExtension) getProject().getExtensions()
                 .getByName(Constants.DUPLICATE_FINDER);
 
+        final ClasspathFilter classpathFilter = new ClasspathFilter(extension);
+
         final Map<String, Set<String>> modulesByFile = new HashMap<>();
         getProject().getConfigurations().stream()
                 .filter(Configuration::isCanBeResolved)
@@ -36,7 +39,7 @@ public class DuplicateFinderTask extends DefaultTask {
                         final Enumeration<? extends ZipEntry> entries = new ZipFile(artifact.getFile()).entries();
                         while (entries.hasMoreElements()) {
                             final ZipEntry entry = entries.nextElement();
-                            if (!entry.isDirectory() && !entry.getName().startsWith("META-INF/")) {
+                            if (!entry.isDirectory() && !classpathFilter.test(entry.getName())) {
                                 final Set<String> modules = modulesByFile.getOrDefault(entry.getName(), new HashSet<>());
                                 modules.add(artifact.getModuleVersion().toString());
                                 modulesByFile.put(entry.getName(), modules);
